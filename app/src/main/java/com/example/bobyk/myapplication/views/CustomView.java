6ede71d51ddcc7e266abf1ff5d1071e817a0943a
EgorBobyk <egor4k.964@gmail.com>
2016-07-29 18:33:45 +0300

    commit

--- app/src/main/java/com/example/bobyk/myapplication/views/CustomView.java
+++ app/src/main/java/com/example/bobyk/myapplication/views/CustomView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -16,6 +17,8 @@ import com.example.bobyk.myapplication.R;
 public class CustomView extends View {
 
     private int color;
+    int height = 0;
+    int width = 0;
     private Paint paint = new Paint();
 
     public CustomView(Context context){
@@ -48,8 +51,30 @@ public class CustomView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle(100, 100, 30, paint);
+
+        int littleRadius = Math.min(height, width) / 20;
+        int bigRadius = Math.min(height, width) / 6;
+
+        int pos1X, pos1Y, pos2X, pos2Y, pos3X, pos3Y, pos4X, pos4Y;
+
+
+        if (height >= width){
+            int diff = (height - width) / 2;
+            canvas.drawCircle(littleRadius, littleRadius + diff, littleRadius, paint);
+            canvas.drawCircle(width - littleRadius, littleRadius + diff, littleRadius, paint);
+            canvas.drawCircle(littleRadius, height - littleRadius - diff, littleRadius, paint);
+            canvas.drawCircle(width - littleRadius, height - littleRadius - diff, littleRadius, paint);
+
+
+            canvas.drawCircle(width / 2, height / 2, bigRadius, paint);
+        }
     }
 
-
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        height = getMeasuredHeight();
+        width = getMeasuredWidth();
+        System.out.println("Width: " + getMeasuredWidth()  + "  Height: " + getMeasuredHeight());
+    }
 }
